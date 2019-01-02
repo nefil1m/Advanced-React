33ea3addf56340bf97234c9f895259eadea71654
Patryk Gurgul <patrykgurgul.it@gmail.com>
2019-01-02 13:20:48 +0100

    episode 6

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,41 @@
 import React, { Component, Fragment } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+
+const theme = {
+  red: '#f00',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offWhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+`;
+
+const Inner = styled.main`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default class Page extends Component {
   render () {
     return (
-      <Fragment>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </Fragment>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
